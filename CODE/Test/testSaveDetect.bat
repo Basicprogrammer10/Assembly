98254ece8bed1aa695a0dfeb97194c7b12787e7f
Connor Slade <turtleslade@gmail.com>
2021-02-13 22:24:09 -0500

    Remove more bat Files

--- CODE/Test/testSaveDetect.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:loop  
-timeout -t 1 >nul  
-for %%i in (tut1.asm) do echo %%~ai|find "a">nul || goto :loop
-echo file was changed
-call Compile.bat tut1.asm true
-attrib -a tut1.asm
-goto :loop
